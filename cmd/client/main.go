9405b5c6c899ac6f3b48bc7ee8522fbd7a3148c7
e1ehpark <ehpark@enterprise1.co.kr>
2024-02-15 15:25:56 +0900

    소스 생성

---
+++ cmd/client/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/e1ehpark/go-kafka-event/event"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func main() {
+	c := flag.Int("c", 1, "number of clients")
+	flag.Parse()
+
+	log.Printf("Starting %d clients", *c)
+
+	consumer := newConsumer()
+	defer consumer.Close()
+
+	err := consumer.SubscribeTopics([]string{"event.OrderProcessedTopic"}, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	stopChan := make(chan struct{})
+
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, os.Interrupt)
+
+		for {
+			select {
+			case <-sigChan:
+				close(stopChan)
+				return
+			default:
+				msg, err := consumer.ReadMessage(100 * time.Millisecond)
+				if err != nil {
+					continue
+				}
+
+				order := event.Order{}
+
+				err = order.UnmarshalBinary(msg.Value)
+				if err != nil {
+					log.Printf("Failed to unmarshal order: %s", err)
+					continue
+				}
+
+				log.Printf("Order ID s% is ready for pickup. Enjoy your coffee!", order.OrderID)
+			}
+		}
+	}()
+
+	client := http.DefaultClient
+
+	for i := 0; i < *c; i++ {
+		go func() {
+			amount := rand.Intn(5) + 1
+			req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("http://localhost:8080/order?amount=%d", amount), nil)
+			if err != nil {
+				panic(err)
+			}
+
+			resp, err := client.Do(req)
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusAccepted {
+				panic("order not accepted")
+			}
+
+			var order event.Order
+
+			err = json.NewDecoder(resp.Body).Decode(&order)
+			if err != nil {
+				panic(err)
+			}
+
+			log.Printf("Order accepted: %s", order.OrderID)
+
+		}()
+
+		time.Sleep(1 * time.Second)
+	}
+
+	<-stopChan
+}
+
+func newConsumer() *kafka.Consumer {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return consumer
+}
---
+++ cmd/counter/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"github.com/e1ehpark/go-kafka-event/event"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+type Counter struct {
+	*http.Server
+	producer    *kafka.Producer
+	deliveryChan chan kafka.Event
+}
+
+func NewCounter(addr string, producer *kafka.Producer) *Counter {
+	c := &Counter{
+		producer: producer,
+		deliveryChan: make(chan kafka.Event),
+	}
+
+	c.Server = &http.Server{
+		Addr:         addr,
+		Handler:      http.HandlerFunc(c.takeOrder),
+	}
+
+	return c
+}
+
+func (c *Counter) takeOrder(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		 c.handleOrder(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (c *Counter) handleOrder(w http>responseWriter,r *http.Request) {
+	amount := r.URL.Query().Get("amount")
+	if amount == "" {
+		http.Error(w, "Missing amount", http.StatusBadRequest)
+		return
+	}
+
+	numAmount, err := strconv.Atoi(amount)
+	if err != nil {
+		http.Error(w, "Invalid amount", http.StatusBadRequest)
+		return
+	}
+
+	if numAmount <= 0 {
+		http.Error(w, "Invalid amount", http.StatusBadRequest)
+		return
+	}
+
+	order := event.NewOrder(numAmount)
+
+	val, err := order.MarshalBinary()
+	if err != nil {
+		http.Error(w, "Failed to marshal order", http.StatusInternalServerError)
+		return
+	}
+
+	err = c.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic: &event.OrderProcessedTopic,
+		Partition: kafka.PartitionAny,
+		},
+		Key:  []byte(order.OrderID),
+		Value:          val,
+	}, c.deliveryChan)
+	if err != nil {
+		http.Error(w, "Failed to produce order", http.StatusInternalServerError)
+		return
+	}
+
+	e := <-c.deliveryChan
+	m := e.(*kafka.Message)
+
+	if m.TopicPartition.Error != nil {
+		http.Error(w, "Failed to deliver order", http.StatusInternalServerError)
+		return
+	} else {
+		log.Printf(w, "Produced message to popic %s [%d] at offset %v\n", 
+			*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+			w.WriteHeader(http.StatusCreated)
+			w.Headerr().Add("Content-Type", "application/json")
+			w.Write(val)
+	}
+}
+
+func main() {
+	producer, errr := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Counter is ready to take orders.")
+
+	stopChan := make(chan struct())
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+
+	counter := NewCounter(":8080", producer)
+
+	go func() {
+		defer close(stopChan)
+		defer close(sigChan)
+		<-sigChan
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		_= counter.Shutdown(ctx)
+	}
+
+	log.Println("Starting counter...")
+
+	err = counter.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		panic(err)
+	}
+
+	<-stopChan
+
+	log.Println("Counter is shutting down")
+}
+
+
+
