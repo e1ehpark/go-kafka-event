433d08b29fafe6d207494a94ceade5c4ace095f6
e1ehpark <ehpark@enterprise1.co.kr>
2024-02-16 15:03:37 +0900

    수정

--- Makefile
+++ Makefile
@@ -1,6 +1,19 @@
 runcounter:
 	go run .\counter
 
+runclient:
+	go run .\client
+
+runevent:
+	go run .\event
+
+runorder:
+	go run .\order
+
+runall:
+	go run .\all
+
+
 installs:
 	go get github.com/confluentinc/confluent-kafka-go/v2/kafka
 	go get github.com/google/uuid
---
+++ barista/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/e1ehpark/go-kafka-event/event"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func main() {
+	consumer := newConsumer()
+	defer consumer.Close()
+
+	log.Println("Barista is ready to take orders")
+
+	producer := newProducer()
+	defer producer.Close()
+
+	log.Println("Barista is ready to deliver orders")
+
+	err := consumer.SubscribeTopics([]string{event.OrderReceivedTopic}, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		// for async writes
+		for e := range producer.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					log.Printf("Delivery failed: %v\n", ev.TopicPartition.Error)
+				} else {
+					log.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+
+	run := true
+
+	for run {
+		select {
+		case <-sigChan:
+			run = false
+		default:
+			msg, err := consumer.ReadMessage(100 * time.Millisecond)
+			if err != nil {
+				continue
+			}
+
+			order := event.Order{}
+			err = order.UnmarshalBinary(msg.Value)
+			if err != nil {
+				log.Printf("Error unmarshalling order: %s\n", err)
+				continue
+			}
+
+			log.Printf("Order received: %s\n", order.OrderID)
+
+			go func() {
+				time.Sleep(time.Duration(order.Amount) * time.Second)
+
+				order.Status = event.OrderProcessed
+
+				val, err := order.MarshalBinary()
+				if err != nil {
+					log.Printf("Error marshalling order: %s\n", err)
+					return
+				}
+
+				err = producer.Produce(&kafka.Message{
+					TopicPartition: kafka.TopicPartition{
+						Topic:     &event.OrderProcessedTopic,
+						Partition: kafka.PartitionAny,
+					},
+					Key:   []byte(order.OrderID),
+					Value: val,
+				}, nil)
+				if err != nil {
+					log.Printf("Error producing order: %s\n", err)
+					return
+				}
+
+				log.Printf("Order processed: %s\n", order.OrderID)
+
+				producer.Flush(15 * 1000)
+			}()
+		}
+	}
+
+	log.Println("Shutting down barista...")
+}
+
+func newConsumer() *kafka.Consumer {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":     "localhost:9092",
+		"group.id":              "barista_group",
+		"auto.offset.reset":     "earliest",
+		"broker.address.family": "v4",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return consumer
+}
+
+func newProducer() *kafka.Producer {
+	producer, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return producer
+}
